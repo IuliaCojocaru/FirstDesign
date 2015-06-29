0ddfabc7260e347ca7b3c68dc2db57b93999f478
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-06-29 16:55:31 +0300

    small changes

--- js/app.js
+++ js/app.js
@@ -1,63 +1,75 @@
 'use strict';
 
-function populateTable(){
+var myApp = {
+    getAllRows: function() {
+        return document.getElementById("table-wrapper").querySelectorAll("div");
+    }
+};
 
-    var elementFromHTML = document.getElementsByClassName("name-column")[0];
-    console.log(elementFromHTML);
-    var elem = elementFromHTML.getAttribute("data-column-type");
-    console.log(elem);
+var myApp = (function() {
+    var getAllRows = function(type){
+        return document.getElementById("table-wrapper").querySelectorAll("div");
+    };
 
-    var res = elem.replace("-column", "");
+    var getOddRows = function() {
+        return getAllRows('odd');
+    };
 
+    var getEvenRows = function() {
+        return getAllRows('even');
+    };
+
+    return {
+        getOddRows: getOddRows,
+        getEvenRows: getEvenRows
+    };
+}());
+
+
+function getAllRows(){
+    return document.getElementById("table-wrapper").querySelectorAll("div");
+}
+
+function fillEachColumn(index) {
+    document.getElementsByClassName('name-column')[index].innerHTML = employees[index].fullname;
+    document.getElementsByClassName('job-column')[index].innerHTML = employees[index].jobtitle;
+    document.getElementsByClassName('grade-column')[index].innerHTML = employees[index].grade;
+    document.getElementsByClassName('status-column')[index].innerHTML = employees[index].allocationstatus;
+    document.getElementsByClassName('project-column')[index].innerHTML = employees[index].project;
+    document.getElementsByClassName('booking-column')[index].innerHTML = employees[index].dateofbooking;
+}
+
+function verifyElementWithProperty(element){
     for(var index in employees){
-        for(var obj in employees[index]){
-              if(obj.indexOf(res) >= 0){
-                  console.log("ok");
-                  document.getElementsByClassName('name-column')[index].innerHTML = employees[index].fullname;
-                  document.getElementsByClassName('job-column')[index].innerHTML = employees[index].jobtitle;
-                  document.getElementsByClassName('grade-column')[index].innerHTML = employees[index].grade;
-                  document.getElementsByClassName('status-column')[index].innerHTML = employees[index].allocationstatus;
-                  document.getElementsByClassName('project-column')[index].innerHTML = employees[index].project;
-                  document.getElementsByClassName('booking-column')[index].innerHTML = employees[index].dateofbooking;
-              }
+        for(var employeeObj in employees[index]){
+            if(employeeObj.indexOf(element) >= 0){
+                fillEachColumn(index);
+            }
         }
     }
+}
+
+function getColumnsAndHTMLElements(rows, columns, elementFromHTML, attribute) {
+    for (var row = 0; row < rows.length; row++) {
+        columns = rows[row].querySelectorAll("span");
+        for (var index = 0; index < columns.length; index++) {
+            elementFromHTML = document.getElementsByClassName(columns[index].className)[0];
+            attribute = elementFromHTML.getAttribute("data-column-type");
+            console.log(attribute);
+        }
+    }
+    return attribute;
+}
+
+
+function populateTable(){
+    var columns = "";
+    var elementFromHTML = "";
+    var attribute = "";
+
+    var rows = getAllRows();
+    attribute = getColumnsAndHTMLElements(rows, columns, elementFromHTML, attribute);
+    verifyElementWithProperty(attribute);
 }
 
 populateTable();
-
-console.log("-----------------");
-
-function populateTable2(){
-    var matches = document.getElementById("table-wrapper").querySelectorAll("span");
-    console.log(matches);
-    console.log("-------------------");
-    var elementFromHTML = "";
-    var res = "";
-
-    for(var index in matches){
-        {
-            elementFromHTML = document.getElementsByClassName(matches[index].className)[0];
-            console.log("Element from html:", elementFromHTML);
-
-            var elem = elementFromHTML.getAttribute("data-column-type");
-
-            console.log("Elem by attribute:" ,elem);
-
-            res = elem.replace("-column", "");
-
-           /* for(var index in employees) {
-                for (var obj in employees[index]) {
-                    if (obj.indexOf(res) >= 0) {
-
-                    }
-                }
-            }*/
-        }
-
-    }
-
-}
-
-
-populateTable2();
