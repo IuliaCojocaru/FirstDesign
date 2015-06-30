cd6de12f411be14665894352d64626732d9f5ebe
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-06-30 11:16:07 +0300

    namespace created

--- js/app.js
+++ js/app.js
@@ -1,75 +1,58 @@
 'use strict';
 
-var myApp = {
-    getAllRows: function() {
+var myApp = (function(){
+
+    var init = function(){
+        bindEvents();
+    };
+
+    var bindEvents = function(){
+        var columns = "";
+        var elementFromHTML = "";
+        var attribute = "";
+
+        var rows = getAllRows();
+        attribute = getColumnsAndHTMLElements(rows, columns, elementFromHTML, attribute);
+        verifyElementWithProperty(attribute);
+    };
+
+    var getAllRows = function(){
         return document.getElementById("table-wrapper").querySelectorAll("div");
-    }
-};
-
-var myApp = (function() {
-    var getAllRows = function(type){
-        return document.getElementById("table-wrapper").querySelectorAll("div");
     };
 
-    var getOddRows = function() {
-        return getAllRows('odd');
+    var fillEachColumn = function(index){
+        document.getElementsByClassName('name-column')[index].innerHTML = employees[index].fullname;
+        document.getElementsByClassName('job-column')[index].innerHTML = employees[index].jobtitle;
+        document.getElementsByClassName('grade-column')[index].innerHTML = employees[index].grade;
+        document.getElementsByClassName('status-column')[index].innerHTML = employees[index].allocationstatus;
+        document.getElementsByClassName('project-column')[index].innerHTML = employees[index].project;
+        document.getElementsByClassName('booking-column')[index].innerHTML = employees[index].dateofbooking;
     };
 
-    var getEvenRows = function() {
-        return getAllRows('even');
-    };
-
-    return {
-        getOddRows: getOddRows,
-        getEvenRows: getEvenRows
-    };
-}());
-
-
-function getAllRows(){
-    return document.getElementById("table-wrapper").querySelectorAll("div");
-}
-
-function fillEachColumn(index) {
-    document.getElementsByClassName('name-column')[index].innerHTML = employees[index].fullname;
-    document.getElementsByClassName('job-column')[index].innerHTML = employees[index].jobtitle;
-    document.getElementsByClassName('grade-column')[index].innerHTML = employees[index].grade;
-    document.getElementsByClassName('status-column')[index].innerHTML = employees[index].allocationstatus;
-    document.getElementsByClassName('project-column')[index].innerHTML = employees[index].project;
-    document.getElementsByClassName('booking-column')[index].innerHTML = employees[index].dateofbooking;
-}
-
-function verifyElementWithProperty(element){
-    for(var index in employees){
-        for(var employeeObj in employees[index]){
-            if(employeeObj.indexOf(element) >= 0){
-                fillEachColumn(index);
+    var verifyElementWithProperty = function(element){
+        for(var index in employees){
+            for(var employeeObj in employees[index]){
+                if(employeeObj.indexOf(element) >= 0){
+                    fillEachColumn(index);
+                }
             }
         }
-    }
-}
+    };
 
-function getColumnsAndHTMLElements(rows, columns, elementFromHTML, attribute) {
-    for (var row = 0; row < rows.length; row++) {
-        columns = rows[row].querySelectorAll("span");
-        for (var index = 0; index < columns.length; index++) {
-            elementFromHTML = document.getElementsByClassName(columns[index].className)[0];
-            attribute = elementFromHTML.getAttribute("data-column-type");
-            console.log(attribute);
+    var getColumnsAndHTMLElements = function(rows, columns, elementFromHTML, attribute) {
+        for (var row = 0; row < rows.length; row++) {
+            columns = rows[row].querySelectorAll("span");
+            for (var index = 0; index < columns.length; index++) {
+                elementFromHTML = document.getElementsByClassName(columns[index].className)[0];
+                attribute = elementFromHTML.getAttribute("data-column-type");
+            }
         }
+        return attribute;
+    };
+
+    return{
+        init: init()
     }
-    return attribute;
-}
+})();
 
 
-function populateTable(){
-    var columns = "";
-    var elementFromHTML = "";
-    var attribute = "";
-
-    var rows = getAllRows();
-    attribute = getColumnsAndHTMLElements(rows, columns, elementFromHTML, attribute);
-    verifyElementWithProperty(attribute);
-}
-
-populateTable();
---
+++ service/data.js
@@ -0,0 +1,47 @@
+/**
+ * Created by icojocaru on 6/25/2015.
+ */
+
+'use strict';
+var employees = [
+    {
+        fullname: "Ciba Mumu",
+        jobtitle: "UI Developer",
+        grade: "Senior Engineer",
+        allocationstatus: "Allocated",
+        project: "Trinity Mirror",
+        dateofbooking: "10/05/2014"
+    },
+    {
+        fullname: "Vrinceanu Valentin",
+        jobtitle: "Java Developer",
+        grade: "Senior Consultant",
+        allocationstatus: "Not Allocated",
+        project: "Visa",
+        dateofbooking: "10/05/2014"
+    },
+    {
+        fullname: "Bogdan Volosincu",
+        jobtitle: ".NET Developer",
+        grade: "Senior Technician",
+        allocationstatus: "Allocated",
+        project: "Graduates",
+        dateofbooking: "10/05/2014"
+    },
+    {
+        fullname: "Ciba Mumu",
+        jobtitle: "UI Developer",
+        grade: "Senior Engineer",
+        allocationstatus: "Not Allocated",
+        project: "Visa",
+        dateofbooking: "10/05/2014"
+    },
+    {
+        fullname: "Carmen Budau",
+        jobtitle: "Business Analyst",
+        grade: "Engineer",
+        allocationstatus: "Not Allocated",
+        project: "Denplan",
+        dateofbooking: "10/05/2014"
+    }
+];
