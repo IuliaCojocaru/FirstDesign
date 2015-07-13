c8423a1026b837286296a25260a5e6a7b9c91e14
IuliaCojocaru <iulia.cojocaru@endava.com>
2015-07-13 14:13:35 +0300

    added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+            files: ['js/*.js'],
+            options: {
+                globals: {
+                }
+            }
+	}
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
+};
